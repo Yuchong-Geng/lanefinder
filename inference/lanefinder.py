92d26d8207ce87456bd957cf02c4eda5e9599cb0
ycgeng <ycgeng@ucdavis.edu>
2020-10-08 13:57:53 -0400

    Update lanefinder.py

--- inference/lanefinder.py
+++ inference/lanefinder.py
@@ -122,7 +122,7 @@ class Lanefinder:
         # We need to set resolutions. 
         # so, convert them from float to integer. 
    
-        size = (640, 360)
+        size = (480, 320)
 
         # Below VideoWriter object will create 
         # a frame of above defined The output  
